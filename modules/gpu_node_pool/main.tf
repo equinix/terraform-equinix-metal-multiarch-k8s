e70edda7559d1cd6a12673afa3e03f6bd0c420e8
Joseph D. Marhee <jmarhee@interiorae.com>
2020-03-09 17:18:01 +0000

    Updates repo to 0.12 syntax

--- modules/gpu_node_pool/main.tf
+++ modules/gpu_node_pool/main.tf
@@ -10,7 +10,7 @@ variable "project_id" {}
 variable "storage" {}
 
 data "template_file" "gpu_node" {
-  template = "${file("${path.module}/gpu_node.tpl")}"
+  template = file("${path.module}/gpu_node.tpl")
 
   vars = {
     kube_token      = "${var.kube_token}"
@@ -21,15 +21,15 @@ data "template_file" "gpu_node" {
 }
 
 resource "packet_device" "gpu_node" {
-  hostname         = "${format("${var.cluster_name}-gpu-${var.pool_label}-%02d", count.index)}"
+  hostname         = format("${var.cluster_name}-gpu-${var.pool_label}-%02d", count.index)
   operating_system = "ubuntu_18_04"
-  count            = "${var.count_gpu}"
-  plan             = "${var.plan_gpu}"
+  count            = var.count_gpu
+  plan             = var.plan_gpu
   facilities       = ["${var.facility}"]
-  user_data        = "${data.template_file.gpu_node.rendered}"
+  user_data        = data.template_file.gpu_node.rendered
   tags             = ["kubernetes", "pool-${var.cluster_name}-${var.pool_label}-gpu"]
 
   billing_cycle = "hourly"
-  project_id    = "${var.project_id}"
+  project_id    = var.project_id
 }
 
