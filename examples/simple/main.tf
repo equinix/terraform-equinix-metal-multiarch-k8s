0334f9f483624918703e6ebb27bb9c595b92d2cd
Marques Johansson <mjohansson@equinix.com>
2022-10-18 17:08:17 -0400

    update simple example to use module version 0.5.0
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -4,7 +4,7 @@ variable "project_id" {} # Or omit to have the module create a project.
 
 module "multiarch-k8s" {
   source  = "equinix/multiarch-k8s/metal"
-  version = "0.1.0" # Use the latest version, according to https://github.com/equinix/terraform-metal-multiarch-k8s/releases
+  version = "0.5.0" # Use the latest version, according to https://github.com/equinix/terraform-metal-multiarch-k8s/releases
 
   # In a production
   auth_token = var.auth_token
