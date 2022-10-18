008db3e97649ec1ddda797aa156b259f036c1e90
Marques Johansson <mjohansson@equinix.com>
2022-10-18 17:32:34 -0400

    update simple example to work in TF 1+
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,16 +1,32 @@
 # main.tf
-variable "auth_token" {}
-variable "project_id" {} # Or omit to have the module create a project.
+variable "auth_token" {
+  type        = string
+  description = "Equinix Metal API key"
+  sensitive   = true
+}
+
+variable "project_id" {
+  type        = string
+  description = "Equinix Metal Project ID"
+}
+
+variable "metro" {
+  type        = string
+  description = "Equinix Metal Metro"
+  default     = "da"
+}
 
 module "multiarch-k8s" {
-  source  = "equinix/multiarch-k8s/metal"
-  version = "0.5.0" # Use the latest version, according to https://github.com/equinix/terraform-metal-multiarch-k8s/releases
+  source = "../.."
+  # source  = "equinix/multiarch-k8s/metal"
+  # version = "0.5.0" # Use the latest version, according to https://github.com/equinix/terraform-metal-multiarch-k8s/releases
 
   # In a production
   auth_token = var.auth_token
   project_id = var.project_id
+  metro      = var.metro
 }
 
-provider "metal" {
+provider "equinix" {
   auth_token = var.auth_token
 }
---
+++ examples/simple/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    equinix = {
+      source  = "equinix/equinix"
+      version = ">= 1.10"
+    }
+  }
+  required_version = ">= 1"
+}
