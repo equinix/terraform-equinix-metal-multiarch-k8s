6a44f389e3178370326bfd19a5a34ef82e98157d
Marques Johansson <mjohansson@equinix.com>
2022-10-19 16:18:02 -0400

    examples/simple should pass cluster_name through
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -28,17 +28,24 @@ variable "count_arm" {
   default     = "0"
 }
 
+variable "cluster_name" {
+  type        = string
+  description = "Name of your cluster. Alpha-numeric and hyphens only, please."
+  default     = "metal-multiarch-k8s"
+}
+
 module "multiarch-k8s" {
   source = "../.."
   # source  = "equinix/multiarch-k8s/metal"
   # version = "0.5.0" # Use the latest version, according to https://github.com/equinix/terraform-metal-multiarch-k8s/releases
 
   # In a production
-  auth_token = var.auth_token
-  project_id = var.project_id
-  metro      = var.metro
-  count_arm  = var.count_arm
-  count_x86  = var.count_x86
+  auth_token   = var.auth_token
+  project_id   = var.project_id
+  metro        = var.metro
+  count_arm    = var.count_arm
+  count_x86    = var.count_x86
+  cluster_name = var.cluster_name
 }
 
 provider "equinix" {
