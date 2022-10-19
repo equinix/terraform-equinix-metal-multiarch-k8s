c1ec76568e307b0bf284df59a063333b7434d422
Marques Johansson <mjohansson@equinix.com>
2022-10-19 16:39:54 -0400

    remove example comment 'in a production'

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -39,7 +39,6 @@ module "multiarch-k8s" {
   # source  = "equinix/multiarch-k8s/metal"
   # version = "0.5.0" # Use the latest version, according to https://github.com/equinix/terraform-metal-multiarch-k8s/releases
 
-  # In a production
   auth_token   = var.auth_token
   project_id   = var.project_id
   metro        = var.metro
